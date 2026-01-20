8dc4e9cd4b490844879d904bdff111a429f07b0c
JavaCommons Technologies <javacommons@gmail.com>
2026-01-20 13:40:14 +0900

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # IExchangeableToObject
+
+[![NuGet version (IExchangeableToObject)](https://img.shields.io/nuget/v/IExchangeableToObject.svg?style=flat-square)](https://www.nuget.org/packages/IExchangeableToObject/)
---
+++ build.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+set -uvx
+set -e
+cd "$(dirname "$0")"
+cwd=`pwd`
+ts=`date "+%Y.%m%d.%H%M.%S"`
+version="${ts}"
+
+cd $cwd
+cp README.md IExchangeableToObject/
+
+cd $cwd
+find . -name bin -exec rm -rf {} +
+find . -name obj -exec rm -rf {} +
+
+cd $cwd
+dotnet test -p:Configuration=Release -p:Platform="Any CPU" IExchangeableToObject.sln
+
+cd $cwd/IExchangeableToObject
+sed -i -e "s/<Version>.*<\/Version>/<Version>${version}<\/Version>/g" IExchangeableToObject.csproj
+rm -rf *.nupkg
+dotnet pack -o . -p:Configuration=Release -p:Platform="Any CPU" IExchangeableToObject.csproj
+
+#exit 0
+
+tag="IExchangeableToObject-v$version"
+cd $cwd
+git add .
+git commit -m"$tag"
+git tag -a "$tag" -m"$tag"
+git push origin "$tag"
+git push origin HEAD:main
+git remote -v
